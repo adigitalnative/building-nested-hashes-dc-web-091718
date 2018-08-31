232de0f973f7bbbf1a15932e13030826082e868f
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-31 23:35:54 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,8 +9,8 @@ def second_challenge
     :capulet => {
       patriarch: [],
       matriarch: [],
-      heronine: [],
-      heronine_friends: []
+      heroine: [],
+      heroine_friends: []
       }}
 
   
