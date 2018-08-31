e2f55451b05302b9e70a8494340db9543f129f43
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-31 23:54:02 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,12 @@ def bonus
 
   #code your solution here:
 
-  
+  def speak_the_speech(epic_tragedy)
+    spoil_the_ending = "dead"
+    
+    epic_tragedy[:montague][:hero][:status] = "Alive"
+    epic_tragedy[:capulet][:heroine][:status] = "Dead"
+  end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
