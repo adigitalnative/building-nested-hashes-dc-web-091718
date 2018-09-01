ace301904e5ba22699eaa055deea4f4dc7af08c2
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-01 00:01:47 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,25 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague",
+        age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet",
+        age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
-        },
+        name: "Lady Capulet",
+        age: "51" #NOTE: THIS IS WRONG. SHE IS ~27 years old. "I became a mother much upon these years that you are now a maid" R&J 1.3.74–75
+      },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet",
+        age: "15", #WRONG! She is 13! "On Lammas-eve, at night, will she be fourteen". The ball takes place a fortnight - two weeks - before her birthday, so mid-July"
+        status: "alive"
         },
       :heroine_friends => []
    }
